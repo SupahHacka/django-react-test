7fea2cae1829a8ee0859ff92132fd10787182511
Inno Ramos <srinno71@gmail.com>
2021-06-17 18:05:03 +0800

    React Router, homepage handling redirection to joinroompage and createroompage

--- music_controller/frontend/src/components/home-page.js
+++ music_controller/frontend/src/components/home-page.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import RoomJoinPage from './room-join-page';
+import CreateRoomPage from './create-room-page';
+import { BrowserRouter as Router, Switch, Route, Link, Redirect } from "react-router-dom"
 
 export default class HomePage extends Component{
     constructor(props){
@@ -6,6 +9,14 @@ export default class HomePage extends Component{
     }
 
     render(){
-        return <p>This is the home page</p>;
+        return( 
+            <Router>
+                <Switch>
+                    <Route exact path="/">This is the home page</Route>
+                    <Route path="/join" component={RoomJoinPage} />
+                    <Route path="/create" component={CreateRoomPage} />
+                </Switch>
+            </Router>
+        );
     }
 }
