076030794e6477812c29ec10f44e2852394880c9
Inno Ramos <srinno71@gmail.com>
2021-06-17 17:50:23 +0800

    Added Extra Components( HomePage,RoomJoinPage & CreateRoomPage )

--- music_controller/frontend/src/components/App.js
+++ music_controller/frontend/src/components/App.js
@@ -1,5 +1,8 @@
 import React, { Component } from "react";
 import { render } from "react-dom";
+import HomePage from "./home-page";
+import RoomJoinPage from "./room-join-page";
+import CreateRoomPage from "./create-room-page";
 
 export default class App extends Component{
     constructor(props){
@@ -7,9 +10,15 @@ export default class App extends Component{
     }
 
     render() {
-        return <h1>Testing React Code</h1>;
+        return (
+        <div>
+            <HomePage />
+            <RoomJoinPage />
+            <CreateRoomPage />
+        </div>
+        );
     }
 }
 
 const appDiv = document.getElementById("app");
-render(<App />, appDiv);
+render(<App name="inno"/>, appDiv);
