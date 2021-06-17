1461594b4128612f2c4836d512861b32efd84819
Inno Ramos <srinno71@gmail.com>
2021-06-17 19:39:06 +0800

    Test accept POST Request by create room app

--- music_controller/api/urls.py
+++ music_controller/api/urls.py
@@ -1,6 +1,8 @@
+from django.http import request
 from django.urls import path
-from .views import RoomView
+from .views import RoomView, CreateRoomView
 
 urlpatterns = [
     path('room', RoomView.as_view()),
+    path('create-room', CreateRoomView.as_view())
 ]
