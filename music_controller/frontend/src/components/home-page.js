e8f49dec48bae836f034a307b27975c9a9f8281f
Inno Ramos <srinno71@gmail.com>
2021-06-19 08:29:18 +0800

    Calling API Endpoints from React frontend

--- music_controller/frontend/src/components/home-page.js
+++ music_controller/frontend/src/components/home-page.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import RoomJoinPage from './room-join-page';
 import CreateRoomPage from './create-room-page';
+import Room from './room';
 import { BrowserRouter as Router, Switch, Route, Link, Redirect } from "react-router-dom"
 
 export default class HomePage extends Component{
@@ -15,6 +16,7 @@ export default class HomePage extends Component{
                     <Route exact path="/">This is the home page</Route>
                     <Route path="/join" component={RoomJoinPage} />
                     <Route path="/create" component={CreateRoomPage} />
+                    <Route path="/room/:roomCode" component={Room} />
                 </Switch>
             </Router>
         );
